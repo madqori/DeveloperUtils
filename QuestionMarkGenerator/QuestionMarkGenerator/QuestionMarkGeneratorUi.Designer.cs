72bd42e0138791b8f2635d882091724c13569ab5
ACER\madqori <mqumaster@gmail.com>
2018-07-27 23:56:44 +0700

    add QuestionMarkGenerator

---
+++ QuestionMarkGenerator/QuestionMarkGenerator/QuestionMarkGeneratorUi.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace QuestionMarkGenerator
+{
+    partial class QuestionMarkGeneratorUi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLineFrom = new System.Windows.Forms.TextBox();
+            this.txtLineTo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLineFrom
+            // 
+            this.txtLineFrom.Location = new System.Drawing.Point(71, 31);
+            this.txtLineFrom.Name = "txtLineFrom";
+            this.txtLineFrom.Size = new System.Drawing.Size(100, 20);
+            this.txtLineFrom.TabIndex = 0;
+            this.txtLineFrom.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLineFrom_KeyPress);
+            // 
+            // txtLineTo
+            // 
+            this.txtLineTo.Location = new System.Drawing.Point(237, 31);
+            this.txtLineTo.Name = "txtLineTo";
+            this.txtLineTo.Size = new System.Drawing.Size(100, 20);
+            this.txtLineTo.TabIndex = 1;
+            this.txtLineTo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLineTo_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "From Line";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "To Line";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(15, 57);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(322, 96);
+            this.txtOutput.TabIndex = 4;
+            this.txtOutput.Text = "";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(15, 2);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(96, 2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 175);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLineTo);
+            this.Controls.Add(this.txtLineFrom);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Question Mark Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLineFrom;
+        private System.Windows.Forms.TextBox txtLineTo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ QuestionMarkGenerator/QuestionMarkGenerator/QuestionMarkGeneratorUi.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuestionMarkGenerator
+{
+    public partial class QuestionMarkGeneratorUi : Form
+    {
+        public QuestionMarkGeneratorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int fromLine = TryParseInt(txtLineFrom.Text);
+                int toLine = TryParseInt(txtLineTo.Text);
+                ValidateData(fromLine, toLine);
+                txtOutput.Text = GenerateParamQuestionMark(fromLine, toLine);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ValidateData(int fromLine, int toLine)
+        {
+            if (fromLine > toLine)
+                throw new Exception("FromLine must be lower than toLine");
+        }
+
+        private string GenerateParamQuestionMark(int fromLine, int toLine)
+        {
+            string output = "(";
+            int range = toLine - fromLine;
+            for (int i = 0; i < range; i++)
+            {
+                output += "?";
+                if (i + 1 == range)
+                    output += ")";
+                else
+                    output += ",";
+            }
+            return output;
+        }
+
+        private int TryParseInt(string text)
+        {
+            try
+            {
+                int result = int.Parse(text);
+                if (result == 0)
+                    throw new Exception("Field must higher than zero");
+
+                return int.Parse(text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return 0;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtLineFrom.Text = "";
+                txtLineTo.Text = "";
+                txtOutput.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+
+
+        private void txtLineFrom_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                ValidateTypeNumberOnly(sender, e);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private static void ValidateTypeNumberOnly(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtLineTo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                ValidateTypeNumberOnly(sender, e);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
